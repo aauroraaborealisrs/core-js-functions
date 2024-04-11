95aa306b2934678e15fc41f403b05a38a67e97d3
aauroraaborealisrs <imaginepeach@mail.ru>
2024-04-11 14:18:26 +0300

    feat: add solution for 7 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -162,8 +162,16 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  function logAndExecute(args) {
+    const message = `${func.name}(${args.map(JSON.stringify).join(',')})`;
+    logFunc(`${message} starts`);
+    const result = func(...args);
+    logFunc(`${message} ends`);
+    return result;
+  }
+
+  return (...args) => logAndExecute(args);
 }
 
 /**
